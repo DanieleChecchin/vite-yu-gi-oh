5c443ce66074eb55de3dc98f989304ad5285a6bd
DanieleChecchin <danielechecchin26@gmail.com>
2024-09-18 14:11:44 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+
+}
+
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+@use "bootstrap/scss/bootstrap.scss" as *;
+@use "./styles/general.scss" as *;
+</style>
