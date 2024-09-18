675c984f126b4ce2cacde5895ed94d5a01d3809d
DanieleChecchin <danielechecchin26@gmail.com>
2024-09-18 17:05:29 +0200

    chiamata API

--- src/components/AppMainCards.vue
+++ src/components/AppMainCards.vue
@@ -1,24 +1,48 @@
 <script>
 
 import AppMainCardsItem from './AppMainCardsItem.vue';
+import axios from 'axios';
 
 export default {
     data() {
         return {
-
+            cardsList: [],
+            apiUrl: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=15&offset=0"
         }
+    },
+    methods: {
+        // Chiamata API
+        getCards() {
+            axios.get(this.apiUrl)
+                .then((response) => {
+                    // handle success
+                    console.log(response);
+                    this.cardsList = response
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                });
+        }
+
     },
     components: {
         AppMainCardsItem
+    },
+    created() {
+        this.getCards();
     }
 }
 </script>
 
 <template>
-    <div>
-        <h1>AppMainCards</h1>
-    </div>
-    <AppMainCardsItem />
+    <section class="container mt-4 bg-white p-5">
+        <div class="row">
+            <div class="col-6 col-lg-3" v-for="n in 15">
+                <AppMainCardsItem />
+            </div>
+        </div>
+    </section>
 </template>
 
 <style lang="scss" scoped></style>
--- src/components/AppMainCardsItem.vue
+++ src/components/AppMainCardsItem.vue
@@ -10,10 +10,13 @@ export default {
 </script>
 
 <template>
-    <article>
-        <img src="" alt="">
-        <h4></h4>
-        <span></span>
+    <article class="card w-100 my-3">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                card's content.</p>
+        </div>
     </article>
 </template>
 
