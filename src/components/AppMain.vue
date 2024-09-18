5aa4536931b99c1928507b596f89e2c4ab30db98
DanieleChecchin <danielechecchin26@gmail.com>
2024-09-18 14:28:21 +0200

    Creato progetto con componenti

---
+++ src/components/AppMain.vue
@@ -0,0 +1,28 @@
+<script>
+
+import AppMainSelect from './AppMainSelect.vue';
+import AppMainCards from './AppMainCards.vue';
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+    components: {
+        AppMainSelect,
+        AppMainCards
+    }
+
+}
+</script>
+
+<template>
+    <div>
+        <h1>Main</h1>
+    </div>
+    <AppMainSelect />
+    <AppMainCards />
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/AppMainCards.vue
@@ -0,0 +1,24 @@
+<script>
+
+import AppMainCardsItem from './AppMainCardsItem.vue';
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+    components: {
+        AppMainCardsItem
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h1>AppMainCards</h1>
+    </div>
+    <AppMainCardsItem />
+</template>
+
+<style lang="scss" scoped></style>
