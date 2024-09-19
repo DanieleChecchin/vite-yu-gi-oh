71d817a9f0a09a33f27d7679068bb6085ccbf1dc
DanieleChecchin <danielechecchin26@gmail.com>
2024-09-19 15:16:06 +0200

    Chiamata API spostata in AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,16 +2,37 @@
 
 import AppMainSelect from './AppMainSelect.vue';
 import AppMainCards from './AppMainCards.vue';
+import axios from 'axios';
 
 export default {
     data() {
         return {
-
+            cardsList: [],
+            apiUrl: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=15&offset=0"
         }
+    },
+    methods: {
+        // Chiamata API
+        getCards() {
+            axios.get(this.apiUrl)
+                .then((response) => {
+                    // handle success
+                    console.log(response.data.data);
+                    this.cardsList = response.data.data;
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                });
+        }
+
     },
     components: {
         AppMainSelect,
         AppMainCards
+    },
+    created() {
+        this.getCards();
     }
 
 }
@@ -20,7 +41,7 @@ export default {
 <template>
     <main class="py-4">
         <AppMainSelect />
-        <AppMainCards />
+        <AppMainCards :cards="cardsList" />
     </main>
 </template>
 
--- src/components/AppMainCards.vue
+++ src/components/AppMainCards.vue
@@ -6,40 +6,26 @@ import axios from 'axios';
 export default {
     data() {
         return {
-            cardsList: [],
-            apiUrl: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=15&offset=0"
-        }
-    },
-    methods: {
-        // Chiamata API
-        getCards() {
-            axios.get(this.apiUrl)
-                .then((response) => {
-                    // handle success
-                    console.log(response.data.data);
-                    this.cardsList = response.data.data;
-                })
-                .catch(function (error) {
-                    // handle error
-                    console.log(error);
-                });
-        }
 
+        }
     },
     components: {
         AppMainCardsItem
     },
-    created() {
-        this.getCards();
+    props: {
+        cards: {
+            type: Array,
+            required: true
+        }
     }
 }
 </script>
 
 <template>
     <section class="container mt-4 bg-white p-5">
-        <div class="counter text-white p-3 fw-bold"> Found {{ cardsList.length }} cards </div>
+        <div class="counter text-white p-3 fw-bold"> Found {{ cards.length }} cards </div>
         <div class="row">
-            <AppMainCardsItem v-for="card in cardsList" :key="card.id" :cardObject="card" />
+            <AppMainCardsItem v-for="card in cards" :key="card.id" :cardObject="card" />
         </div>
     </section>
 </template>
